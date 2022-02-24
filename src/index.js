331fbc8645cadedbfcd19c2a753745044c8899ef
brianko <kobj11@gmail.com>
2022-02-24 11:32:03 -0500

    phase-1-project

--- src/index.js
+++ src/index.js
@@ -2,52 +2,52 @@ let addDrink = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   getDrinks()
-    const addBtn = document.querySelector("#new-drink-btn");
-    const drinkFormContainer = document.querySelector(".container");
-    addBtn.addEventListener("click", () => {
-      
-      addDrink = !addDrink;
-      if (addDrink) {
-        drinkFormContainer.style.display = "block";
-      } else {
-        drinkFormContainer.style.display = "none";
-      }
-    });
+
+  const addBtn = document.querySelector("#new-drink-btn");
+  const drinkFormContainer = document.querySelector(".container");
+  addBtn.addEventListener("click", () => {
+    
+    addDrink = !addDrink;
+    if (addDrink) {
+      drinkFormContainer.style.display = "block";
+    } else {
+      drinkFormContainer.style.display = "none";
+    }
   });
+  document.querySelector('.add-drink-form').addEventListener('submit', handleSubmit)
+});
 
-  function renderDrinks(drink) {
+function renderDrinks(drink) {
+  const foo      = drink
+  let card = document.createElement('li')
+  card.className="card"
+  card.innerHTML = `
 
-    let card = document.createElement('li')
-    card.className="card"
-    card.innerHTML = `
-  
-      <img src="${drink.image}" class="card-img">
-      <div class="content">
+    <img src="${drink.image}" class="card-img">
+    <div class="content">
       <h4>${drink.name}</h4>
       <p>${drink.preparation}</p>
-      </div>
-    
-    `
-    document.querySelector('#drink-collection').appendChild(card)
-
-  }
-
-  function getDrinks(){
-      fetch('http://localhost:3000/cocktails')
-      .then(res => res.json())
-      .then(drinks => drinks.forEach(drink => renderDrinks(drink)))
-  }
-
-  document.querySelector('.add-drink-form').addEventListener('submit', handleSubmit)
+    </div>
   
-  function handleSubmit(e){
-      e.preventDefault()
+  `
+  document.querySelector('#drink-collection').appendChild(card)
+}
 
-      let drinkObj = {
+function getDrinks(){
+  fetch('http://localhost:3000/cocktails')
+  .then(res => res.json())
+  .then(drinks => drinks.forEach(drink => renderDrinks(drink)))
+}
 
-        image:e.target.querySelector("#inputDrinkImg").value,
-        name:e.target.querySelector("#inputDrink").value,
-        preparation:e.target.querySelector("#recipe").value
-      }
-      renderDrinks(drinkObj)
+
+function handleSubmit(e){
+  e.preventDefault()
+
+  let drinkObj = {
+
+    image:e.target.querySelector("#inputDrinkImg").value,
+    name:e.target.querySelector("#inputDrink").value,
+    preparation:e.target.querySelector("#recipe").value
   }
+  renderDrinks(drinkObj)
+}
